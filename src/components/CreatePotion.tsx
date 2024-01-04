07dd38751d7e153d31e28fc69a4ef55e89002a9a
josef256 <hanchaoui.youssef256@gmail.com>
2024-01-04 19:38:38 +0100

    add task wip

--- src/components/CreatePotion.tsx
+++ src/components/CreatePotion.tsx
@@ -1,16 +1,51 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import {
+	Text,
+	View,
+	ScrollView,
+	StyleSheet,
+	Pressable,
+	TextInput,
+} from "react-native";
 import DateTimePicker from "react-native-ui-datepicker";
+import ConfirmIcon from "../../assets/confirm-icon.svg";
+import RejectIcon from "../../assets/reject-icon.svg";
+
 export default function CreatePotion(): JSX.Element {
+	const [isCalendarVisible, setIsCalendarVisible] = useState(false);
+	const [isInputVisible, setIsInputVisible] = useState(false);
+	const [selectedDate, setSelectedDate] = useState("");
+	const datePress = () => {
+		setIsCalendarVisible(!isCalendarVisible);
+	};
 	return (
 		<View style={style.container}>
-			<View style={style.content}>
+			<ScrollView
+				contentContainerStyle={style.contentContainer}
+				style={style.content}
+			>
 				<Text>Add Task</Text>
 				<View>
-					<Text>Select a date</Text>
-					<View style={style.datepicker}></View>
+					<Pressable onPress={datePress}>
+						<Text>Select a date</Text>
+					</Pressable>
+					<View style={style.datepicker}>
+						{isCalendarVisible && <DateTimePicker />}
+					</View>
 				</View>
-				<View></View>
-			</View>
+				<View>
+					<View>
+						<Text>Add Task</Text>
+					</View>
+					<View style={style.inputContainer}>
+						<TextInput style={style.input} />
+						<View style={style.actionButtonContainer}>
+							<ConfirmIcon width={30} height={30} />
+							<RejectIcon width={30} height={30} />
+						</View>
+					</View>
+				</View>
+			</ScrollView>
 		</View>
 	);
 }
@@ -24,11 +59,32 @@ const style = StyleSheet.create({
 		justifyContent: "center",
 		alignItems: "center",
 	},
-	content: {
+	contentContainer: {
 		padding: 15,
 		backgroundColor: "white",
-		width: "90%",
 		minHeight: "50%",
 	},
+	content: {
+		width: "90%",
+		maxHeight: "50%",
+		backgroundColor: "white",
+	},
 	datepicker: {},
+	inputContainer: {
+		justifyContent: "space-between",
+		alignItems:"center",
+		flexDirection: "row",
+
+	},
+	actionButtonContainer: {
+		justifyContent: "space-between",
+		alignItems:"center",
+		flexDirection: "row",
+		gap:15
+	},
+	input: {
+		borderWidth: 1,
+		backgroundColor:"red",
+		width: "70%",
+	},
 });
